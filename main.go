07ccf1c159459c17bda5e3ada2cc3cf3309659a3
Tezan Sahu <tezansahu@gmail.com>
2019-05-07 10:33:52 +0530

    Implemented PoW Algorithm

--- main.go
+++ main.go
@@ -1,54 +1,25 @@
 package main
 
-import(
-	"bytes"
-	"crypto/sha256"
+import (
 	"fmt"
+	"strconv"
+
+	"github.com/tezansahu/golang_blockchain/blockchain"
 )
 
-type Blockchain struct{
-	blocks []*Block
-}
-
-type Block struct{
-	Hash 		[]byte
-	Data 		[]byte
-	PrevHash	[]byte
-}
-
-func (b *Block) DeriveHash(){
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
-}
-
-func (chain *Blockchain) AddBlock(data string){
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new :=CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
-}
-func Genesis() *Block{
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockchain() *Blockchain{
-	return &Blockchain{[]*Block{Genesis()}}
-}
-
-func main(){
-	chain :=InitBlockchain()
+func main() {
+	chain := blockchain.InitBlockchain()
 	chain.AddBlock("First Block")
 	chain.AddBlock("Second Block")
 
-	for _,block := range(chain.blocks){
+	for _, block := range chain.Blocks {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n\n", block.Hash)
-	}	
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("Nonce: %d\n", block.Nonce)
+
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+	}
 }
